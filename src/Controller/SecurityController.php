18dcf80ab20f1048edbd4db3495487f595848536
adam <adam.grzelec@gmail.com>
2026-02-19 00:06:18 +0100

    login frontend

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,8 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -12,33 +10,18 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
-    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('app_ranking_page');
         }
 
-        if ($request->isMethod('POST')) {
-            return new JsonResponse(['message' => 'Invalid credentials.'], Response::HTTP_UNAUTHORIZED);
-        }
-
-        if ($request->isXmlHttpRequest() || str_contains((string) $request->headers->get('Accept'), 'application/json')) {
-            return new JsonResponse([
-                'message' => 'Authenticate by POSTing credentials to /login.',
-                'fields' => [
-                    'email_or__username',
-                    'password_or__password',
-                ],
-            ]);
-        }
-
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return new Response(sprintf(
-            "Login form endpoint ready.\nLast username: %s\nError: %s\n",
-            $lastUsername !== '' ? $lastUsername : '-',
-            $error?->getMessageKey() ?? '-',
-        ));
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 }
