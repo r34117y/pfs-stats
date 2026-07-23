77b52e95ff5bd2bfefd7bd49ec8996b05736df0d
adam <adam.grzelec@gmail.com>
2026-07-23 15:03:50 +0200

    remove legacy references from tournament list service

--- src/Service/TournamentList/TournamentListServicePostgres.php
+++ src/Service/TournamentList/TournamentListServicePostgres.php
@@ -11,8 +11,6 @@ use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 final readonly class TournamentListServicePostgres implements TournamentListServiceInterface
 {
-    private const int TOURNAMENT_ID_WITH_TRAILING_SPACE = 201204220;
-
     public function __construct(
         #[Autowire(service: 'doctrine.dbal.default_connection')]
         private Connection $connection,
@@ -26,19 +24,16 @@ final readonly class TournamentListServicePostgres implements TournamentListServ
     {
         $sql = "SELECT
                     t.id,
-                    t.legacy_id,
                     t.name,
                     t.start_round AS start,
                     t.trank,
                     t.players_count AS players,
                     p.name_show,
-                    t.legacy_winner_player_id AS winner
+                    t.winner_player_id AS winner
                 FROM tournament t
                 INNER JOIN player p ON p.id = t.winner_player_id
                 WHERE t.organization_id = :organizationId
-                  AND t.legacy_id IS NOT NULL
-                  AND t.legacy_winner_player_id IS NOT NULL
-                ORDER BY t.legacy_id DESC";
+                ORDER BY t.id DESC";
         $result = $this->connection->executeQuery($sql, ['organizationId' => $organizationId]);
         $rows = $result->fetchAllAssociative();
         $tournaments = [];
@@ -46,9 +41,6 @@ final readonly class TournamentListServicePostgres implements TournamentListServ
         foreach ($rows as $row) {
             $startDate = DateTime::createFromFormat('Ymd', (string) $row['start']);
             $name = (string) ($row['name'] ?? '');
-            if ((int) $row['legacy_id'] === self::TOURNAMENT_ID_WITH_TRAILING_SPACE && !str_ends_with($name, ' ')) {
-                $name .= ' ';
-            }
 
             $tournaments[] = new TournamentsListTournament(
                 (int) $row['id'],
